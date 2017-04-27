a0af00baae9622184724a55e1cd02261414b1441
frodostark <frodostark@gandalfthegrey>
2017-04-27 16:08:26 -0300

    Trabalho JSF com Rest

--- evento-ejb/src/main/java/br/com/progweb/evento/entitys/Evento.java
+++ evento-ejb/src/main/java/br/com/progweb/evento/entitys/Evento.java
@@ -47,6 +47,7 @@ public class Evento implements Serializable {
 	public void setData(String data) {
 		this.data = data;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -54,6 +55,7 @@ public class Evento implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -70,9 +72,9 @@ public class Evento implements Serializable {
 			return false;
 		return true;
 	}
+	
 	@Override
 	public String toString() {
 		return "Evento [id=" + id + ", nomeLocal=" + nomeLocal + ", duracao=" + duracao + ", data=" + data + "]";
 	}
-
 }
